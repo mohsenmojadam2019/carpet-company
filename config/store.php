d499ac7dc0125ec9ceff63c6d9c65f2a21bd53d0
Mohsen Mojaddam محسن مجدم <56688766+mohsenmojadam2019@users.noreply.github.com>
2026-09-10 00:16:38 +0330

    feat: add store configuration

---
+++ config/store.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'price_unit' => env('STORE_PRICE_UNIT', 'IRT'),
+    'shipping_flat' => (int) env('STORE_SHIPPING_FLAT', 0),
+    'brand' => env('STORE_BRAND', 'خانه فرش'),
+    'phone' => env('STORE_PHONE', '02100000000'),
+    'instagram' => env('STORE_INSTAGRAM'),
+];
