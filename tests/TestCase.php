e21e4d735a5367a44bdf2682eb4115557f5467e3
Mohsen Mojaddam محسن مجدم <56688766+mohsenmojadam2019@users.noreply.github.com>
2026-09-10 00:30:49 +0330

    test: add Laravel test case

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    // Laravel 13 application bootstrapping is discovered from bootstrap/app.php.
+}
